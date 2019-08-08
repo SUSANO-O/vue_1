e21cf3899f375d5afddd2630390c8ac1dab68388
Harold <harold.vargas@bewe.io>
2019-08-08 18:04:46 -0500

    sin stock

--- 01.js
+++ 01.js
@@ -3,10 +3,10 @@ const app = new Vue({
     data: {
         titulo: 'hola mundo de vue',
         frutas: [
-            {nombre:'pera', cantidad:10},
+            {nombre:'pera', cantidad:0},
             {nombre:'manzana',cantidad:0},
             {nombre:'uva', cantidad:0},
-            {nombre:'naranja',cantidad:10}
+            {nombre:'naranja',cantidad:0}
         ],
         message: 'cargaste esta pagina en'   + new Date().toLocaleString()        
     }
